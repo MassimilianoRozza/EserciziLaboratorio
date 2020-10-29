a6b526a9fbc8a80fb277faf5ff16ca7cb6706a69
MassimilianoRozza <rozza.ma.01@gmail.com>
2020-10-29 13:51:03 +0100

    29/10/2020

--- Esercizi1/1.6_Esponenziale/mainEsponenziale.cpp
+++ Esercizi1/1.6_Esponenziale/mainEsponenziale.cpp
@@ -4,6 +4,24 @@
 
 using namespace std;
 
+long double NEulero(int & appros);
+
 int main(){
+    int approssimazione;
     
+    cout<<"inserire numero di addendi da usare per l'approssimazione"<<endl;
+    cin>>approssimazione;
+    long double e = NEulero(approssimazione);
+    cout<<"costante   e="<<exp(1)<<endl;
+    cout<<"calcolato: e="<<e<<endl;
+    cout<<endl<<"differenza = "<<exp(1)-e<<endl;
+}
+
+long double NEulero(int & appros){
+    long double e=1;
+    for(appros; appros>0;appros--){
+        cout<<(1/fattoriale(appros));
+        e=e+(1/fattoriale(appros));
+    }
+    return e;
 }
