2d55440ba5ec10fe3244bfac607072900f6b5183
MassimilianoRozza <rozza.ma.01@gmail.com>
2020-11-02 21:26:59 +0100

    completamento esercizio 0.9

--- Esercizi0/0.9_TabellaAscii.cpp
+++ Esercizi0/0.9_TabellaAscii.cpp
@@ -8,17 +8,48 @@ using namespace std;
 #include <iostream>
 int main(int argc, char const *argv[])
 {
-    for (int a=65; a<=90; a++)
+    /*
+    + assegno alla variabile a il numero corrispondente
+        alla lettera "a";
+    + poi nel ciclo aumento il valore della variabile 
+        finché il valore di a è minore o uguale al numero 
+        corrispondente alla lettera "z";
+    */
+    for (int a=char('a'); a<=char('z'); a++)
+    {
+        /*
+        stampo "CHAR: <carattere>     ASCII: <numero-corrispondente-al-carattere>
+        */
+        cout<<"CHAR:     "<<char(a)<<"   ASCII:   "<<a<<endl;
+    }
+    /*
+    separo con una linea orizzontale la stampa delle lettere 
+    minuscole dalle successive.
+    */
+    cout<<endl<<"=========================================================================================================================="<<endl<<endl;
+    /*
+    + assegno alla variabile a il numero corrispondente
+        alla lettera "A";
+    + poi nel ciclo aumento il valore della variabile 
+        finché il valore di a è minore o uguale al numero 
+        corrispondente alla lettera "Z";
+    */
+    for (int a=char('A'); a<=char('Z'); a++)
     {
         cout<<"CHAR:     "<<char(a)<<"   ASCII:   "<<a<<endl;
     }
+
+    //separatore
     cout<<endl<<"=========================================================================================================================="<<endl<<endl;
-    for (int a=97; a<=122; a++)
-    {
-        cout<<"CHAR:     "<<char(a)<<"   ASCII:   "<<a<<endl;
-    }
-    cout<<endl<<"=========================================================================================================================="<<endl<<endl;
-    for (int a=48; a<=57; a++)
+    
+    /*
+    + assegno alla variabile a il numero corrispondente
+        al carattere "0";
+    + poi nel ciclo aumento il valore della variabile 
+        finché il valore di a è minore o uguale al numero 
+        corrispondente al carattere "9";
+    */
+    for (int a=char('0'); a<=char('9'); a++)
     {
         cout<<"CHAR:     "<<char(a)<<"   ASCII:   "<<a<<endl;
     }
